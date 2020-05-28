e85a68922d813ca7cb1b6d909e2cbdc37e36ad41
Et7f3 <cadeaudeelie@gmail.com>
2020-05-28 15:33:05 +0200

    wtf: Try to guess why bs doesn't output nything on stderr.

--- src/ocamlc_opt.ml
+++ src/ocamlc_opt.ml
@@ -17,26 +17,39 @@ let () =
     else
     let () = av.(0) <- bsc in
     let (bs_stdout, _bs_stdin, bs_stderr) = Unix.open_process_args_full bsc av [||] in
-    let out = open_out (output_file_name ^ ".js") in
+    let output_file_name = output_file_name ^ ".js" in
+    let out = open_out output_file_name in
     let () = print_endline ("bsc: " ^ bsc) in
     let () = Printf.printf "output_file_name: %s\n" output_file_name in
     let () = Printf.printf "cd %s && " (Sys.getcwd ()) in
     let () = Array.iter (fun e -> Printf.printf "%s %!" e) av in
+    let () = output_char stdout '\n' in
     let rec loop () =
         try
-            let line = input_line bs_stdout ^ "\n" in
+            let line = really_input_string bs_stdout 1 in
             let () = Lib.my_print_endline line in
+            let () = print_string line in
             let () = output_string out line in
             loop ()
         with End_of_file  -> close_out out
     in
     let () = loop () in
+    let error = ref false in
     let rec loop () =
         try
-            let () = output_string stderr ((input_line bs_stderr) ^ "\n") in
+            let err_line = really_input_string bs_stderr 1 in
+            let () = Lib.my_prerr_endline err_line in
+            let () = print_endline "Hy\n\n\n\n" in
+            let () = output_string stderr err_line in
+            let () = error := true in
             loop ()
         with End_of_file -> ()
-    in Unix.rename output_file_name (Filename.remove_extension output_file_name ^ ".cmo")
+    in let () = Printf.printf "error: %b%!" !error in
+    if not !error then
+        Unix.link output_file_name (Filename.remove_extension output_file_name ^ ".cmo")
+    else
+        let () = prerr_endline "failure\n\n" in
+        exit (-1)
 
 let () = Lib.my_end ()
 
